c5defcdd72867e6643796da3a6fcbdd33bb4b153
Caue Polimanti <caue.polimanti@gmail.com>
2017-03-09 01:40:27 -0300

    Obtendo o BD MySQL de uma váriavel de ambiente ou arquivo local

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 // This application uses express as its web server
 // for more info, see: http://expressjs.com
 var express = require('express');
-
+var fs = require('fs');
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
 var cfenv = require('cfenv');
@@ -14,30 +14,15 @@ var bodyParser = require('body-parser');
 // configuring the MySQL DB
 var mysql = require('mysql');
 
-// mysql pool connection configuration
-var mySqlPool = mysql.createPool({
-  host: 'us-cdbr-iron-east-03.cleardb.net',
-  user: 'bf581ab6083cb1',
-  password: '1979175d',
-  database: 'ad_2f6fb0f5141bb38',
-  connectionLimit: 4  
-});
+var mySqlDB; 
 
-// local config
-// var mySqlPool = mysql.createPool({
-//   host: 'localhost',
-//   user: 'root',
-//   password: '',
-//   database: 'splitterdb',
-//   connectionLimit: 4  
-// });
+var dbCredentials = {
+    dbName: 'splitterdb'
+};
 
 // create a new express server
 var app = express();
 
-// importing routes
-// passing mySqlPool connection
-var userRouter = require('./src/routes/userRoutes.js')(mySqlPool);
 
 /*
     setting up the middleware
@@ -54,7 +39,55 @@ app.use(bodyParser.urlencoded({extended: false}));
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
 
-console.log(appEnv.cleardb);
+function getDBCredentialsUrl(jsonData) {
+    var vcapServices = JSON.parse(jsonData);
+    // Pattern match to find the first instance of a ClearDB (MySQL) service in
+    // VCAP_SERVICES. If you know your service key, you can access the
+    // service credentials directly by using the vcapServices object.
+    for (var vcapService in vcapServices) {
+        if (vcapService.match(/cleardb/i)) {
+            return vcapServices[vcapService][0].credentials.uri;
+        }
+    }
+}
+
+function initDBConnection() {
+    //When running on Bluemix, this variable will be set to a json object
+    //containing all the service credentials of all the bound services
+    if (process.env.VCAP_SERVICES) {
+        dbCredentials.uri = getDBCredentialsUrl(process.env.VCAP_SERVICES);
+    } else { //When running locally, the VCAP_SERVICES will not be set
+
+        // When running this app locally you can get your MySQL credentials
+        // from Bluemix (VCAP_SERVICES in "cf env" output or the Environment
+        // Variables section for an app in the Bluemix console dashboard).
+        // Once you have the credentials, paste them into a file called vcap-local.json.
+        // Alternately you could point to a local database here instead of a
+        // Bluemix service.
+        // uri will be in this format: mysql://root:@localhost:3306/splitterdb?reconnect=true
+        dbCredentials.uri = getDBCredentialsUrl(fs.readFileSync("vcap-local.json", "utf-8"));
+        console.log(dbCredentials.uri);
+    }
+
+    mySqlDB = mysql.createPool(dbCredentials.uri);
+}
+
+initDBConnection();
+
+// local config
+// var mySqlPool = mysql.createPool({
+//   host: 'localhost',
+//   user: 'root',
+//   password: '',
+//   database: 'splitterdb',
+//   connectionLimit: 4  
+// });
+
+
+// importing routes
+// passing mySqlPool connection
+var userRouter = require('./src/routes/userRoutes.js')(mySqlDB);
+
 // setting the views directory
 app.set('views', './src/views');
 // setting ejs as the view engine
