ef92c31e6639c77293c1d16a96d22945b4fb5c8d
Cauê Polimanti <caue.polimanti@gmail.com>
2017-04-29 14:49:58 -0300

    Alteração para diferente estatégia de BD
    A estatégia de gerenciamento da conexão com o BD foi alterada para utilizar um middleware do express para gerenciar as conexões e queries

--- app.js
+++ app.js
@@ -1,58 +1,53 @@
 /*eslint-env node*/
 
-// This application uses express as its web server
-// for more info, see: http://expressjs.com
+/* Webserver*/
+// express webserver
 var express = require('express');
-var fs = require('fs');
-// cfenv provides access to your Cloud Foundry environment
-// for more info, see: https://www.npmjs.com/package/cfenv
-var cfenv = require('cfenv');
-
-// morgan for logging http requests
-var morgan = require('morgan'); 
-
 // package to parse the body of a request into a JSON object
 var bodyParser = require('body-parser');
+// create a new express server
+var app = express();
+var expressValidator = require('express-validator');
 
+/* Cloud environment */
+// cfenv provides access to your Cloud Foundry environment
+var cfenv = require('cfenv');
+// get the app environment from Cloud Foundry
+var appEnv = cfenv.getAppEnv();
+
+var fs = require('fs');
+
+// morgan for logging http requests
+var morgan = require('morgan');
+
+/*Set EJS template Engine*/
+// setting the views directory
+app.set('views', './src/views');
+// setting ejs as the view engine
+app.set('view engine', 'ejs');
+
+// setting the public directory for static files
+app.use(express.static('public'));
+app.use(bodyParser.json()); // parse application/json (parse the body into req.body for json encoded)
+app.use(bodyParser.urlencoded({
+    extended: true
+})); // parse application/x-www-form-urlencoded (parse the body into req.body for url encoded)
+app.use(expressValidator());
+
+/*MySql connection*/
 // configuring the MySQL DB
 var mysql = require('mysql');
+var connection = require('express-myconnection');
 
-var mySqlDB; 
-
+// Old DB strategy (without a middleware) - kept because being used by user, checkin and cardapio controller
+var mySqlDB;
 var dbCredentials = {
     dbName: 'splitterdb'
 };
 
-// create a new express server
-var app = express();
-
-/*
-    setting up the middleware
-*/
-
-// setting the public directory for static files
-app.use(express.static('public'));
-
-// parse application/json (parse the body into req.body for json encoded)
-app.use(bodyParser.json());
-// parse application/x-www-form-urlencoded (parse the body into req.body for url encoded)
-app.use(bodyParser.urlencoded({extended: false}));
-
-// get the app environment from Cloud Foundry
-var appEnv = cfenv.getAppEnv();
-
-function getDBCredentialsUrl(jsonData) {
-    var vcapServices = JSON.parse(jsonData);
-    // Pattern match to find the first instance of a ClearDB (MySQL) service in
-    // VCAP_SERVICES. If you know your service key, you can access the
-    // service credentials directly by using the vcapServices object.
-    for (var vcapService in vcapServices) {
-        if (vcapService.match(/cleardb/i)) {
-            return vcapServices[vcapService][0].credentials.uri;
-        }
-    }
-}
-
+/**
+ * Initialize DB connection
+ */
 function initDBConnection() {
     //When running on Bluemix, this variable will be set to a json object
     //containing all the service credentials of all the bound services
@@ -73,10 +68,37 @@ function initDBConnection() {
         app.use(morgan('dev'));
     }
 
+    // Old DB strategy pool
     mySqlDB = mysql.createPool(dbCredentials.uri);
+
+    // new DB strategy
+    app.use(
+        connection(mysql, dbCredentials.uri, 'pool')
+    );
+};
+
+/**
+ * Get DB credentials from a local file (vcap-local.json) or Cloud environment variable
+ * @param {*} jsonData 
+ */
+function getDBCredentialsUrl(jsonData) {
+    var vcapServices = JSON.parse(jsonData);
+    // Pattern match to find the first instance of a ClearDB (MySQL) service in
+    // VCAP_SERVICES. If you know your service key, you can access the
+    // service credentials directly by using the vcapServices object.
+    for (var vcapService in vcapServices) {
+        if (vcapService.match(/cleardb/i)) {
+            return vcapServices[vcapService][0].credentials.uri;
+        }
+    }
 }
 
+//initiate db connection
 initDBConnection();
+
+/*
+    setting up the middleware
+*/
 
 // importing routes
 // passing mySqlPool connection
@@ -85,11 +107,6 @@ var checkinRouter = require('./src/routes/checkinRoutes')(mySqlDB);
 var cardapioRouter = require('./src/routes/cardapioRoutes')(mySqlDB);
 var pedidoRouter = require('./src/routes/pedidoRoutes')();
 
-// setting the views directory
-app.set('views', './src/views');
-// setting ejs as the view engine
-app.set('view engine', 'ejs');
-
 // using the routes
 app.use('/api/usuario', userRouter);
 app.use('/api/checkin', checkinRouter);
@@ -97,9 +114,9 @@ app.use('/api/cardapio', cardapioRouter);
 app.use('/api/pedido', pedidoRouter);
 
 // start server on the specified port and binding host
-app.listen(appEnv.port, '0.0.0.0', function() {
-  // print a message when the server starts listening
-  console.log("server starting on " + appEnv.url);
+app.listen(appEnv.port, '0.0.0.0', function () {
+    // print a message when the server starts listening
+    console.log("server starting on " + appEnv.url);
 });
 
 // exporting app to be executed on supertest
