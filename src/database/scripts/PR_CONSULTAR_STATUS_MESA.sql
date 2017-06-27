a46187b5629a004847d6407985db306413b7819e
Cauê Polimanti <caue.polimanti@gmail.com>
2017-06-27 11:10:36 -0300

    Adicionada PR consultar status mesa

---
+++ src/database/scripts/PR_CONSULTAR_STATUS_MESA.sql
@@ -0,0 +1,50 @@
+-- CREATE PROCEDURE
+DROP PROCEDURE IF EXISTS pr_consultar_status_mesa;
+
+DELIMITER $$
+CREATE PROCEDURE pr_consultar_status_mesa (
+IN cod_mesa INT,
+IN id_associacao_estabelecimento VARCHAR(50)
+)
+ 
+BEGIN 
+
+	SET @ind_status_mesa = (SELECT A.ind_status_mesa
+							FROM tb_mesa AS A
+							INNER JOIN tb_estabelecimento AS B
+							ON A.cod_estabelecimento = B.cod_estabelecimento
+							INNER JOIN tb_status_mesa AS C
+							ON A.ind_status_mesa = C.ind_status_mesa
+							WHERE A.cod_mesa = cod_mesa
+							  AND B.id_associacao_estabelecimento = id_associacao_estabelecimento);
+							 
+	IF (@ind_status_mesa = 0) THEN						 
+
+		SELECT C.dsc_ind_status_mesa, cod_qr_ocupado, Null as cod_cliente
+		FROM tb_mesa AS A
+		INNER JOIN tb_estabelecimento AS B
+		ON A.cod_estabelecimento = B.cod_estabelecimento
+		INNER JOIN tb_status_mesa AS C
+		ON A.ind_status_mesa = C.ind_status_mesa
+		WHERE A.cod_mesa = cod_mesa
+		  AND B.id_associacao_estabelecimento = id_associacao_estabelecimento;
+	  
+	  
+	ELSE
+	
+		SELECT C.dsc_ind_status_mesa, cod_qr_ocupado, E.txt_email
+		FROM tb_mesa AS A
+		INNER JOIN tb_estabelecimento AS B
+		ON A.cod_estabelecimento = B.cod_estabelecimento
+		INNER JOIN tb_status_mesa AS C
+		ON A.ind_status_mesa = C.ind_status_mesa
+		INNER JOIN tb_cliente AS E 
+				ON A.id = E.id
+		WHERE A.cod_mesa = cod_mesa
+		  AND B.id_associacao_estabelecimento = id_associacao_estabelecimento;
+
+	END IF;
+
+END $$
+
+DELIMITER ;	
